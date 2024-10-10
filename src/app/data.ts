6270b0517458c97539ed41f8a6fd6b9f071a36dd
demmynile <balogun80@gmail.com>
2024-10-10 20:44:54 +0100

    add new data into the system

--- src/app/data.ts
+++ src/app/data.ts
@@ -5,6 +5,24 @@ import tSpeech from "../../public/images/t-speech.jpeg"
 export const projectData: allProjectProps = [
   {
     id : 1,
+    title: "Crime Prediction with Machine Learning: A Step Towards Safer Communities(BLOG POST)",
+    image: tSpeech,
+    github:
+      "https://medium.com/@balogunademola_51837/crime-prediction-with-machine-learning-a-step-towards-safer-communities-384291f1dfdb",
+    liveLink: "",
+    description: "",
+  },
+  {
+    id : 2,
+    title: "Churn Prediction Using Machine Learning: A Step-by-Step Guide(BLOG POST)",
+    image: tSpeech,
+    github:
+      "https://medium.com/@balogunademola_51837/churn-prediction-using-machine-learning-a-step-by-step-guide-3380afe5652f",
+    liveLink: "",
+    description: "",
+  },
+  {
+    id : 3,
     title: "Quiz LLM",
     image: tSpeech,
     github:
@@ -13,7 +31,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 2,
+    id : 4,
     title: "SASS CHAT USING LLM",
     image: tSpeech,
     github:
@@ -22,7 +40,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 3,
+    id : 5,
     title: "OPEN SOURCE LLM RAG APPLICATIONS",
     image: tSpeech,
     github:
@@ -31,7 +49,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 4,
+    id : 6,
     title: "Churn Prediction using Machine Learning",
     image: churnImage,
     github:
@@ -40,7 +58,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 5,
+    id : 7,
     title: "Car Price Prediction using Machine Learning and Deep Learning Models",
     image: churnImage,
     github: "https://github.com/Demmynile/Car-price-prediction/blob/main/CAR%20PRICE%20PREDICTION%20(SUMMATIVE%20ASSIGNNMENT).ipynb",
@@ -48,7 +66,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 6,
+    id : 9,
     title: "Credit Score Rating Algorithms",
     image: churnImage,
     github: "https://github.com/Demmynile/Journey_to_ML_engineer/blob/main/machine-learning/credit-risk-scoring/Credit-scoring-risk.ipynb",
