7e4d6ece33d4ad64b59eea962f1a1e9e59589df3
demmynile <balogun80@gmail.com>
2024-10-17 11:55:15 +0100

    add new commit to the repo

--- src/app/data.ts
+++ src/app/data.ts
@@ -3,26 +3,9 @@ import churnImage from "../../public/images/Churn-Prediction-scaled.jpg";
 import tSpeech from "../../public/images/t-speech.jpeg"
 
 export const projectData: allProjectProps = [
+  
   {
     id : 1,
-    title: "Crime Prediction with Machine Learning: A Step Towards Safer Communities(BLOG POST)",
-    image: tSpeech,
-    github:
-      "https://medium.com/@balogunademola_51837/crime-prediction-with-machine-learning-a-step-towards-safer-communities-384291f1dfdb",
-    liveLink: "",
-    description: "",
-  },
-  {
-    id : 2,
-    title: "Churn Prediction Using Machine Learning: A Step-by-Step Guide(BLOG POST)",
-    image: tSpeech,
-    github:
-      "https://medium.com/@balogunademola_51837/churn-prediction-using-machine-learning-a-step-by-step-guide-3380afe5652f",
-    liveLink: "",
-    description: "",
-  },
-  {
-    id : 3,
     title: "Quiz LLM",
     image: tSpeech,
     github:
@@ -31,7 +14,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 4,
+    id : 2,
     title: "SASS CHAT USING LLM",
     image: tSpeech,
     github:
@@ -40,7 +23,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 5,
+    id : 3,
     title: "OPEN SOURCE LLM RAG APPLICATIONS",
     image: tSpeech,
     github:
@@ -49,7 +32,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 6,
+    id : 4,
     title: "Churn Prediction using Machine Learning",
     image: churnImage,
     github:
@@ -58,7 +41,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 7,
+    id : 5,
     title: "Car Price Prediction using Machine Learning and Deep Learning Models",
     image: churnImage,
     github: "https://github.com/Demmynile/Car-price-prediction/blob/main/CAR%20PRICE%20PREDICTION%20(SUMMATIVE%20ASSIGNNMENT).ipynb",
@@ -66,7 +49,7 @@ export const projectData: allProjectProps = [
     description: "",
   },
   {
-    id : 9,
+    id : 6,
     title: "Credit Score Rating Algorithms",
     image: churnImage,
     github: "https://github.com/Demmynile/Journey_to_ML_engineer/blob/main/machine-learning/credit-risk-scoring/Credit-scoring-risk.ipynb",
